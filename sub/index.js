bd8241ec0bc5d6673f76907cdac9f7bd00e79755
OliverNEIRINCKX <83227735+OliverNEIRINCKX@users.noreply.github.com>
2021-10-19 19:15:44 +0200

    Update index.js

--- sub/index.js
+++ sub/index.js
@@ -1,3 +1,20 @@
-fetch('https://jsonplaceholder.typicode.com/todos/3')
-		.then(resp => resp.json())
-		.then(todo => document.getElementById("demo").innerHTML = JSON.stringify(todo))
+
+
+ fetch('https://jsonplaceholder.typicode.com/todos')
+		        .then(resp => resp.json())
+                .then(todo => {
+                    let text = "";
+                    for (let x in todo) {
+                        let thing = todo[x];
+                        for (let y in thing) {
+                            text += thing[y] + "<br>"
+                        }
+                        text += "<br>"
+                    }
+                    return text;
+                })
+                .then(todo => document.getElementById("demo").innerHTML= todo)
+
+       
+
+ 
