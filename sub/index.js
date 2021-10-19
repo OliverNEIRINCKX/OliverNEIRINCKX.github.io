2433240b6857b09a9bfb4a4e57acea5fb129dee9
OliverNEIRINCKX <83227735+OliverNEIRINCKX@users.noreply.github.com>
2021-10-19 19:23:30 +0200

    Update index.js

--- sub/index.js
+++ sub/index.js
@@ -4,13 +4,17 @@
 		        .then(resp => resp.json())
                 .then(todo => {
                     let text = "";
+
                     for (let x in todo) {
                         let thing = todo[x];
+
                         for (let y in thing) {
                             text += thing[y] + "<br>"
                         }
+
                         text += "<br>"
                     }
+
                     return text;
                 })
                 .then(todo => document.getElementById("demo").innerHTML= todo)
